ad14d9344f7ecaa03e62d92101fc6e5b01b6e8e8
ivenkateswarlu <ivenkateswarlu777@gmail.com>
2023-02-09 17:20:24 -0600

    Added map and destructuring to main.js

--- main.js
+++ main.js
@@ -50,24 +50,30 @@ let shopItems=[
     },
 ];
 let generateShop = () => {
-    return shop.innerHTML=`
-    <div class="item">
-    <img width="244" src="images/image1.jpg" alt="img1" >
-    <div class="details">
-        <h3>T-Shirt</h3>
-        <p>This is a premium quality T-shirt which doesn't have any artifical colors in it.</p>
-        <div class="price-quantity">
-            <h2>$10</h2>
-            <div class="buttons">
-                <i class="bi bi-dash-lg"></i>
-                <div class="quantity">0</div>
-                <i class="bi bi-plus-lg"></i>
+    return (shop.innerHTML=shopItems
+        .map((x)=>{
+            let{id,name,price,desc,img}=x
+        return `
+        <div id=product-id-${id} class="item">
+        <img width="244" src=${img} alt="img1" >
+        <div class="details">
+            <h3>${name}</h3>
+            <p>${desc}</p>
+            <div class="price-quantity">
+                <h2>$ ${price}</h2>
+                <div class="buttons">
+                    <i class="bi bi-dash-lg"></i>
+                    <div id=${id}class="quantity">0</div>
+                    <i class="bi bi-plus-lg"></i>
+                </div>
             </div>
         </div>
     </div>
-</div>
+    
+        `
 
-    `
+    })
+    .join(""));
 
 
 };
