dc1303a84b808cc6e2b282bc6c2d1ffc396f403f
ivenkateswarlu <ivenkateswarlu777@gmail.com>
2023-02-10 16:47:33 -0600

    Added functionality for clearcart,remv item,amount

--- src/cart.js
+++ src/cart.js
@@ -19,18 +19,25 @@ let genrateCartItems = () =>{
                 let search=shopItems.find((y) => y.id === id) || [];
             return `
             <div class="cart-item">
-            <img width = "150" src="${search.img}" alt=""/>
+            <img width = "100" src="${search.img}" alt=""/>
             <div class="details">
-                <div className="title-price-x">
-                    <h4>
+                <div onclick = "removeItem(${id})"class="title-price-x">
+                    <h4 class="title-price">
                         <p>${search.name}</p>
-                        <p>$ ${search.price}</p>
+                        <p class="cart-item-price">$ ${search.price}</p>
+
                     </h4>
                     <i class="bi bi-x-lg"></i>
                 </div>
 
-                <div className="cart-buttons"></div>
-                <h3></h3>               
+                <div class="buttons">
+                    <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                    <div id=${id} class="quantity">${item}</div>
+                    <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+                </div> 
+
+
+                <h3>$ ${item * search.price}</h3>               
                 </div>
             </div>
             
@@ -56,3 +63,90 @@ let genrateCartItems = () =>{
 
 genrateCartItems();
 
+let increment = (id) => {
+    let selectedItem=id;
+    let search=basket.find((x) => x.id === selectedItem.id);
+    if(search===undefined){
+        basket.push({
+            id:selectedItem.id,
+            item:1,
+        });
+
+    }else{
+        search.item+=1; 
+    }
+   
+    //console.log(basket);
+    update(selectedItem.id);
+    basket=basket.filter((x) => x.item!== 0);
+    genrateCartItems();
+    localStorage.setItem("data",JSON.stringify(basket));
+
+};
+
+let decrement = (id) => {
+    let selectedItem=id;
+    let search=basket.find((x) => x.id ===selectedItem.id );
+    if(search === undefined) return;
+    else if(search.item===0) return;
+    else{
+        search.item-=1;
+    }
+    
+    update(selectedItem.id);
+    basket=basket.filter((x) => x.item!== 0);
+    genrateCartItems();
+    localStorage.setItem("data",JSON.stringify(basket));
+    
+
+};
+
+
+let update = (id) => {
+    let search=basket.find((x) => x.id===id);
+
+    //console.log(search.item);
+    document.getElementById(id).innerHTML=search.item;
+    calculation();
+    totalAmount();
+
+};
+
+let removeItem=(id) =>{
+    let selectedItem=id;
+    basket=basket.filter((x) =>x.id !==selectedItem.id);
+    genrateCartItems();
+    totalAmount();
+    calculation();
+    localStorage.setItem("data",JSON.stringify(basket));
+
+};
+
+let clearCart = () =>{
+    basket=[];
+    genrateCartItems();
+    calculation();
+    localStorage.setItem("data",JSON.stringify(basket));
+};
+
+let totalAmount= () => {
+    if(basket.length!==0){
+
+        let amount=basket.map((x) =>{
+            let {item,id}=x;
+            let search=shopItems.find((y) => y.id === id) || [];
+            return item * search.price;
+
+        }).reduce((x,y) => x+y,0)
+        label.innerHTML=`
+            <h2>Total Bill: $ ${amount}</h2>
+            <button class="checkout"> CheckOut</button>
+            <button onclick = "clearCart()"class="removeall">ClearCart</button>
+        
+        `;
+
+    } 
+    else return
+};
+
+totalAmount();
