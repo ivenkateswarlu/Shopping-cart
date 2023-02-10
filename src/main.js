d7d8e7a74f739ccc004b8739596af7990a31231a
ivenkateswarlu <ivenkateswarlu777@gmail.com>
2023-02-10 15:16:44 -0600

    moving main.js and styling.css to src with update

--- src/main.js
+++ src/main.js
@@ -1,54 +1,6 @@
 let shop=document.getElementById("shop")
 
-let shopItems=[
-    {
 
-        id:"hdbghebgj",
-        name:"T-Shirt",
-        price:10,
-        desc:"This is a premium quality T-shirt which doesn't have any artifical colors in it.",
-        img:"images/image1.jpg"
-
-    },
-    {
-        id:"jfejejf",
-        name:"Blazer",
-        price:25,
-        desc:"This is a premium quality T-shirt which doesn't have any artifical colors in it.",
-        img:"images/Blazer.jpg"
-
-    },
-    {
-        id:"dnfjnefhu",
-        name:"Festival Blazer",
-        price:30,
-        desc:"This is a premium quality T-shirt which doesn't have any artifical colors in it.",
-        img:"images/Festival_Blazer.jpg"
-        
-    },
-    {
-        id:"knfjenf",
-        name:"Formal Shirt",
-        price:15,
-        desc:"This is a premium quality T-shirt which doesn't have any artifical colors in it.",
-        img:"images/formal_shirt.jpg"
-    },
-    {
-        id:"dkvnjnv",
-        name:"Hooded Swatshirt",
-        price:20,
-        desc:"This is a premium quality T-shirt which doesn't have any artifical colors in it.",
-        img:"images/Hooded_Sweatshirt.jpg"
-    },
-    {
-        id:"dsjknjnehugh",
-        name:"Running Shoes",
-        price:20,
-        desc:"This is a premium quality T-shirt which doesn't have any artifical colors in it.",
-        img:"images/Running_Shoes.jpg"
-
-    },
-];
 
 let basket=JSON.parse(localStorage.getItem("data")) || [];
 
