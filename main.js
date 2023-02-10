6fcae3f28e1530e801c2f710408dae272c754193
ivenkateswarlu <ivenkateswarlu777@gmail.com>
2023-02-10 11:35:54 -0600

    localstorage and reflecting cart count

--- main.js
+++ main.js
@@ -50,14 +50,15 @@ let shopItems=[
     },
 ];
 
-let basket=[];
+let basket=JSON.parse(localStorage.getItem("data")) || [];
 
 let generateShop = () => {
     return (shop.innerHTML=shopItems
         .map((x)=>{
-            let{id,name,price,desc,img}=x
+            let{id,name,price,desc,img}=x;
+            let search=basket.find((x) =>x.id === id) || [];
         return `
-        <div id=product-id-${id} class="item">
+    <div id=product-id-${id} class="item">
         <img width="244" src=${img} alt="img1" >
         <div class="details">
             <h3>${name}</h3>
@@ -66,7 +67,9 @@ let generateShop = () => {
                 <h2>$ ${price}</h2>
                 <div class="buttons">
                     <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                    <div id=${id} class="quantity">0</div>
+                    <div id=${id} class="quantity">
+                    ${search.item === undefined ? 0 : search.item}
+                    </div>
                     <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
                 </div>
             </div>
@@ -98,21 +101,27 @@ let increment = (id) => {
     }
    
     //console.log(basket);
-
     update(selectedItem.id);
+    basket=basket.filter((x) => x.item!== 0);
+    
+    localStorage.setItem("data",JSON.stringify(basket));
 
 };
 
 let decrement = (id) => {
     let selectedItem=id;
     let search=basket.find((x) => x.id ===selectedItem.id );
-    if(search.item===0) return;
+    if(search === undefined) return;
+    else if(search.item===0) return;
     else{
         search.item-=1;
     }
     //console.log(basket);
 
     update(selectedItem.id);
+    basket=basket.filter((x) => x.item!== 0);
+    
+    localStorage.setItem("data",JSON.stringify(basket));
 
 };
 
@@ -130,3 +139,5 @@ let calculation= () => {
     cartIcon.innerHTML=basket.map((x) => x.item).reduce((x,y)=> x+y,0 );
 
 };
+
+calculation();
